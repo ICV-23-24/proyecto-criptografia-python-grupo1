b8a5dd2f28c14b056aedce87c3af1c5df1bbaa4c
migatomax123 <kevin.acupar18@gmail.com>
2023-11-15 12:34:05 +0100

    futuos cambios

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 from flask import Flask, render_template, request
 import functions as f
-
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from base64 import b64encode, b64decode
 app = Flask(__name__)
 
 
@@ -58,3 +62,74 @@ def hello_there(name = None):
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
+
+#comienzo del cifrado hibrido
+
+
+def generate_key_pair():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+    public_key = private_key.public_key()
+    return private_key, public_key
+
+def encrypt_with_rsa(public_key, message):
+    ciphertext = public_key.encrypt(
+        message.encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return b64encode(ciphertext).decode()
+
+def decrypt_with_rsa(private_key, ciphertext):
+    decrypted_message = private_key.decrypt(
+        b64decode(ciphertext),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return decrypted_message.decode()
+
+def encrypt_with_aes(key, message):
+    cipher = Cipher(algorithms.AES(key), modes.CFB(), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(message.encode()) + encryptor.finalize()
+    return b64encode(ciphertext).decode()
+
+def decrypt_with_aes(key, ciphertext):
+    cipher = Cipher(algorithms.AES(key), modes.CFB(), backend=default_backend())
+    decryptor = cipher.decryptor()
+    decrypted_message = decryptor.update(b64decode(ciphertext)) + decryptor.finalize()
+    return decrypted_message.decode()
+
+# Uso del cifrado híbrido
+private_key, public_key = generate_key_pair()
+
+message_to_encrypt = "Hola, este es un mensaje secreto."
+
+# Cifrado asimétrico con RSA
+encrypted_message_rsa = encrypt_with_rsa(public_key, message_to_encrypt)
+print("Mensaje cifrado con RSA:", encrypted_message_rsa)
+
+# Generación de una clave simétrica para el cifrado simétrico
+symmetric_key = b"UnaClaveSimetrica"
+print("Clave simétrica generada:", symmetric_key)
+
+# Cifrado simétrico con AES
+encrypted_message_aes = encrypt_with_aes(symmetric_key, message_to_encrypt)
+print("Mensaje cifrado con AES:", encrypted_message_aes)
+
+# Descifrado simétrico con AES
+decrypted_message_aes = decrypt_with_aes(symmetric_key, encrypted_message_aes)
+print("Mensaje descifrado con AES:", decrypted_message_aes)
+
+# Descifrado asimétrico con RSA
+decrypted_message_rsa = decrypt_with_rsa(private_key, encrypted_message_rsa)
+print("Mensaje descifrado con RSA:", decrypted_message_rsa)
