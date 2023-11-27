1f5a266f43ea736cb3c9b70bcf29bd9484538270
Allgames88 <martinjosemorata@gmail.com>
2023-11-27 09:32:58 +0100

    Finalized Database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, request, send_file
+from flask import Flask, json, render_template, request, send_file, jsonify
 import functions as f
 import zipfile
 import os
@@ -54,6 +54,67 @@ def database():
 
             # Enviamos a descargar las claves.
             return send_file(zipFile, as_attachment=True, download_name="keys.zip", mimetype="application/zip")
+        #Si se genera una petición POST, con el modo de 'download', entonces el usuario quiere descargar el archivo que se especifica en el select, o eliminarlo, interactuar con este.
+        elif mode == "download":
+            # Obtenemos el tipo de acción (Descargar o Eliminar), y el nombre del archivo con el que se interactua.
+            fileName = request.form['filename']
+            type = request.form['type']
+            print("requested a " +  type + " for file " + fileName )
+            # Si tipo es 'get' entonces el usuario quiere descargar el archivo.
+            if type=="get":
+                return send_file(os.path.join("static","public_keys",fileName),download_name=fileName,mimetype="application/text")
+            # Si el tipo es 'remove', entonces el usuario quiere eliminar el archivo.
+            if type=="remove":
+                passwd = request.form['passwd']
+                print("Password Inputed: " + passwd)
+                with open("static/data.json","r") as jsonData:
+                    data = json.load(jsonData)
+                    correct = data.get("adminPasswd")
+                if passwd == correct:
+                    answer = f.removeFile("public_keys/"+fileName)
+                    print(answer)
+        elif mode == "load":
+            print("Usuario quiere cargar archivo.")
+            # Obtén las variables necesarias:
+            Id = request.form['identifier']
+            file = request.files['file']
+            # Comprobamos si el archivo con este nuevo nombre, existe o no entre las claves guardadas.
+            # Además, en la misma función, comprobamos si el archivo es una clave exportada u otro archivo random no deseado.
+            if f.canBeLoaded(Id, file, "static/public_keys"):
+                print("File can be loaded to system")
+                file.seek(0)  # Mover el puntero al principio del archivo
+
+                 # Leer el contenido del archivo y eliminar los caracteres de nueva línea
+                file_content = file.read().decode('utf-8').strip()
+
+                # Imprimir el contenido (opcional)
+                print("File content:", file_content)
+
+                # Escribir el contenido en un nuevo archivo
+                with open(f"static/public_keys/{Id}.key", "w", encoding="utf-8") as filed:
+                    filed.write(file_content)
+
+            else:
+                print("File is not allowed in the system")
+
+
+
+                    
+                
+
+        
+    # Si se genera una petición GET, significa que la aplicación requiere de obtener una lista string con las claves públicas del servidor
+    if request.method == 'GET':
+        # Obtiene los nombres de los archivos
+        keys = f.load_keys()
+        
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            # Si es una solicitud Ajax, devuelve las claves como JSON
+            return jsonify({"claves": keys})
+        else:
+            # Si es una solicitud normal, renderiza la plantilla HTML
+            return render_template("database.html", claves=keys)
+
 
 
     return render_template("database.html")
--- functions.py
+++ functions.py
@@ -5,6 +5,8 @@ from base64 import b64encode, b64decode
 # Necesario para hacer funcionar la descarga de archivos.
 import os
 
+from flask import jsonify
+
 
 def encrypt_message(message, key):
     key = key.encode('utf-8')
@@ -26,15 +28,85 @@ def generate_keys():
     # Obtiene la clave privada
     privateKey = key.export_key('PEM')
 
+    # Si la ruta static/temp no existe, la crea
     tempDir = os.path.join("static","temp")
     os.makedirs(tempDir, exist_ok=True)
 
+    # Crea las rutas de los archivos.
     publicFile = os.path.join(tempDir, "public_key.key")
     privateFile = os.path.join(tempDir, "private_key.key")
 
+    # Escribe los archivos
     with open(publicFile, "w") as file:
         file.write(publicKey.decode("utf-8"))
 
     with open(privateFile, "w") as file:
         file.write(privateKey.decode("utf-8"))
 
+
+def load_keys():
+    #Directiorio de claves
+    keys_dir = "static/public_keys"
+
+    fileNames = os.listdir(keys_dir)
+
+    return fileNames
+
+def removeFile(relative_from_static):
+    file_path = os.path.join("static",relative_from_static)
+    try:
+        os.remove(file_path)
+        return jsonify({"message": "Archivo eliminado exitosamente."})
+    except FileNotFoundError:
+        return jsonify({"message": "Archivo no encontrado."})
+    except Exception as e:
+        return jsonify({"error": f"Error al eliminar el archivo: {str(e)}"})
+
+
+
+## Funciones de validación de archivos.
+# Esta función decide si un nuevo archivo .key, puede ser añadido al servidor, o si no cubre con todos los requisitos.
+def canBeLoaded(ID,file, route):
+    genName = ID + ".key"
+    # Comprobamos que el nuevo archivo no existe.
+    if genName in load_keys():
+        return False
+    
+    # Comprobamos que el archivo es una clave pública.
+    if not isPublic_key(file.read()):
+        return False
+    
+    # Comprobamos que el archivo no es muy grande:
+    if isBig(file, 1):
+        return False
+
+
+    return True
+
+
+# Función utilizada para strings, para comprobar si son calves públicas o no.
+def isPublic_key(content):
+    # Si estos strings se encuentran en el archivo, sabemos que es una clave pública
+    if b"BEGIN PUBLIC KEY" in content and b"END PUBLIC KEY" in content:
+        return True
+    
+    ##
+    # Suggestion: Otra manera de la que se podría hacer, sería, trata de cargar la clave al arnillo de confianza del servidor,
+    # Si funciona, entonces la clave es pública y apta, no un txt con eso escrito y ya.
+    # Si da error, entonces no es ni siquiera una clave.
+    ##
+
+    return False
+
+# Función utilizada para comprobar si el tamaño de los archivos es muy grande.
+def isBig(file, maxSize):
+    # Obtén el tamaño del búfer del objeto BytesIO en bytes.
+    size = len(file.getbuffer())
+    print("Tamaño de archivo en bytes: " + str(size))
+    # Lo pasamos a Kilobytes.
+    sizeK = size/1024
+    # Si es mayor a la cantidad determinada, entonces el archivo SI es muy grande.
+    if sizeK > maxSize:
+        return True
+    
+    return False
