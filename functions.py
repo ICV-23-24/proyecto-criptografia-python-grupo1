98eb235db30c28c7d28f34ea6d11b2398dc074a9
migatomax123 <kevin.acupar18@gmail.com>
2023-11-29 14:05:25 +0100

    multiples cambios

--- functions.py
+++ functions.py
@@ -2,6 +2,8 @@ from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import pad,unpad
 from base64 import b64encode, b64decode
 
+#encriptar el mensaje y se espera que la key sea en formato texto
+
 def encrypt_message(message, key):
     key = key.encode('utf-8')
     cipher = AES.new(pad(key, AES.block_size), AES.MODE_ECB)
@@ -12,4 +14,4 @@ def decrypt_message(message, key):
     key = key.encode('utf-8')
     cipher = AES.new(pad(key, AES.block_size), AES.MODE_ECB)
     decrypted_message = unpad(cipher.decrypt(b64decode(message)), AES.block_size).decode('utf-8')
-    return decrypted_message
+    return decrypted_messagecd
